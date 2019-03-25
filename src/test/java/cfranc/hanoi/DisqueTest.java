96f41d5dbb5d52e704d1a7ab0c5010892cfa3ba0
De Brito Cyril <developpeur@cyrildebrito.info>
2019-03-25 17:24:56 +0100

    CDB - Tests Tour and Disk

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -6,14 +6,37 @@ import org.junit.Test;
 
 public class DisqueTest {
 
+	Disque small = new Disque(1);
+	Disque small2 = new Disque(1);
+	Disque medium = new Disque(2);
+	
 	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+	public void compareTo_Small_To_Medium_negative() {
+		//GIVEN
+		int expected = -1;
+		//WHEN
+		int actual = this.small.compareTo(this.medium);
+		//THEN
+		assertEquals(expected, actual);
 	}
-
+	
+	@Test
+	public void compareTo_Medium_To_Small_negative() {
+		//GIVEN
+		int expected = 1;
+		//WHEN
+		int actual = this.medium.compareTo(this.small);
+		//THEN
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void compareTo_Small_To_Small_negative() {
+		//GIVEN
+		int expected = 0;
+		//WHEN
+		int actual = this.small.compareTo(this.small2);
+		//THEN
+		assertEquals(expected, actual);
+	}
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -5,10 +5,116 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TourTest {
+	// HauteurMax des tours
+	Tour t = new Tour(3);
 
 	@Test
-	public void test() {
-
+	public void emplier_TourVide_True() {
+		//GIVEN
+		//WHEN
+		boolean condition = t.estVide();
+		//THEN
+		assertTrue(condition);
 	}
-
+	
+	@Test
+	public void emplier_TourPlein_True() {
+		//GIVEN
+		Disque d1 = new Disque (1);
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		//WHEN
+		t.empiler(d3);
+		t.empiler(d2);
+		t.empiler(d1);
+		boolean condition = t.estPleine();
+		//THEN
+		assertTrue(condition);
+	}
+	
+	@Test
+	public void emplier_TourLibre_True() {
+		//GIVEN
+		Disque d1 = new Disque (1);
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		//WHEN
+		this.t.empiler(d3);
+		this.t.empiler(d2);
+		boolean condition = t.empiler(d1);
+		//THEN
+		assertTrue(condition);
+	}
+	
+	@Test
+	public void emplier_TourFull_False() {
+		//GIVEN
+		Disque d1 = new Disque (1);
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		Disque d4 = new Disque (4);
+		//WHEN
+		t.empiler(d4);
+		t.empiler(d3);
+		t.empiler(d2);
+		boolean condition = t.empiler(d1);
+		//THEN
+		assertFalse(condition);
+	}
+	
+	@Test
+	public void estPleine_true() {
+		//GIVEN
+		Disque d1 = new Disque (1);
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		//WHEN
+		t.empiler(d3);
+		t.empiler(d2);
+		t.empiler(d1);
+		boolean condition = t.estPleine();
+		//THEN
+		assertTrue(condition);
+	}
+	
+	@Test
+	public void estPleine_false() {
+		//GIVEN
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		//WHEN
+		t.empiler(d3);
+		t.empiler(d2);
+		boolean condition = t.estPleine();
+		//THEN
+		assertFalse(condition);
+	}
+	
+	@Test
+	public void diam_DiametreDisqueSommet_Null() {
+		//GIVEN
+		// ----> No disk set is normal here! <----
+		//WHEN
+		int expected = Integer.MAX_VALUE;
+		int actual = t.diam();
+		//THEN
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void diam_DiametreDisqueSommet_NotNull() {
+		//GIVEN
+		Disque d1 = new Disque (1);
+		Disque d2 = new Disque (2);
+		Disque d3 = new Disque (3);
+		//WHEN
+		t.empiler(d3);
+		t.empiler(d2);
+		t.empiler(d1);
+		
+		int expected = 3;
+		int actual = t.diam();
+		//THEN
+		assertEquals(expected, actual);
+	}
 }
